3edef657ff44945b664d6ddc49f0d4cbddf54d91
Marcin Kaczmarek <mkc@orgadata.com>
2016-08-12 09:57:44 +0200

    Fix typos in heald.h

--- src/lib/heald.h
+++ src/lib/heald.h
@@ -1,6 +1,10 @@
 #ifndef KLETCH_LIB_HEALD_H
 #define KLETCH_LIB_HEALD_H
 
+#include "prefix.h"
+
+namespace kletch {
+
 // Standard Fresnel integral evaluation based on
 //
 //     Mark A. Heald, Rational Approximations for the Fresnel Integrals,
@@ -10,13 +14,15 @@ struct Heald
 {
     // Evaluates the standard Fresnel integral `INT t = 0..s: exp(0.5 i pi t^2)` with the nominator
     // of the `R` function of degree 5.
-    vec2r eval5(real S);
+    static vec2r eval5(real S);
 };
 
 // Proxy for `Heald::eval5` that enables it for `FresnelCore`.
 struct Heald5
 {
-    static vec2r eval(real S) { return Heald::eval5(s); }
+    static vec2r eval(real s) { return Heald::eval5(s); }
 };
+
+} // namespace kletch
 
 #endif // KLETCH_LIB_HEALD_H
